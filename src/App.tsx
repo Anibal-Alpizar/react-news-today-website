25ac7591ca8ef9dcf70804438fc4234a330a2a02
anibal-alpizar <anibal.alpizar14@gmail.com>
2023-04-12 22:01:37 -0600

    navbar and fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
-import React from "react";
-
+import ButtonAppBar from "./components/NavBar";
 function App() {
-  console.log(import.meta.env.VITE_API_KEY);
-  return <div>App</div>;
-}
+  const url =
+    "https://newsapi.org/v2/top-headlines?" +
+    "sortBy=popularity&" +
+    `country=us&apiKey=${import.meta.env.VITE_API_KEY}`;
+
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => console.log(data))
+    .catch((error) => console.log(error));
+
+  return <div> <ButtonAppBar /> </div>;}
 
 export default App;
